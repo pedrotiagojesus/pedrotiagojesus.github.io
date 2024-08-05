fc8d72312316870ac9610df93a9bcfab480519f6
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-08-05 15:28:06 +0100

    Remove CDN FontAwesome

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
 
+// Fontawesome
+import "./assets/fontawesome-free-6.6.0-web/css/all.min.css";
+
 function App() {
     return (
         <>
