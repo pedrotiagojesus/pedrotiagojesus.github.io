23f0749d9ebae3019d9de04d0f7a3c520a40571a
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-09-24 00:12:17 +0100

    Add an animated background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
+import BackgroundAnimated from "./components/BackgroundAnimated/BackgroundAnimated";
 
 function App() {
     return (
         <>
+            <BackgroundAnimated />
             <Header />
             <Menu />
             <main>
---
+++ src/components/BackgroundAnimated/BackgroundAnimated.jsx
@@ -0,0 +1,7 @@
+import "./BackgroundAnimated.css";
+
+const BackgroundAnimated = () => {
+    return <div id="background-animated"></div>;
+};
+
+export default BackgroundAnimated;
