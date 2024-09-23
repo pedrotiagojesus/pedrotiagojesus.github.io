a83b8df1244b043d95f28dcee073b77348c9b611
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-09-23 23:52:14 +0100

    Add the functionality of "See more" in long text

--- src/components/Project/Item.jsx
+++ src/components/Project/Item.jsx
@@ -1,7 +1,35 @@
+import { useState, useRef, useEffect } from "react";
+
 // CSS
 import "./Project.css";
 
 const Item = ({ name, description, url }) => {
+    const [showAll, setShowAll] = useState(false);
+    const [showButton, setShowButton] = useState(false);
+
+    const descriptionRef = useRef(null);
+
+    useEffect(() => {
+        const verifyLine = () => {
+            const element = descriptionRef.current;
+            if (element) {
+                const lineHeight = parseFloat(
+                    getComputedStyle(element).lineHeight
+                );
+
+                const maxHeight = lineHeight * 3;
+
+                if (element.scrollHeight > maxHeight) {
+                    setShowButton(true);
+                }
+            }
+        };
+
+        verifyLine();
+        window.addEventListener("resize", verifyLine);
+        return () => window.removeEventListener("resize", verifyLine);
+    }, []);
+
     return (
         <div className="item">
             <div className="heading">
@@ -10,7 +38,22 @@ const Item = ({ name, description, url }) => {
                     {name}
                 </a>
             </div>
-            <div className="description">{description}</div>
+            <div
+                className={`description ${
+                    showButton && !showAll ? "truncate" : ""
+                }`}
+                ref={descriptionRef}
+            >
+                {description}
+            </div>
+            {showButton && !showAll && (
+                <button
+                    className="btn see-more"
+                    onClick={() => setShowAll(!showAll)}
+                >
+                    ver mais
+                </button>
+            )}
         </div>
     );
 };
