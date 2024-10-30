7fdf0b5034dce690ac7fae774b4b8a95882c61d4
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-10-30 17:57:14 +0000

    Clear CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router";
+
+// components
 import BackgroundAnimated from "./components/BackgroundAnimated/BackgroundAnimated";
 import Header from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
