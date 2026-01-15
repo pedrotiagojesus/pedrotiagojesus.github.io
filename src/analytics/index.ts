1c87ec29fdb29b7eda5c7b88fe8235b106b813f8
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2026-01-15 23:35:50 +0000

    Add Google Analytics integration and tracking events

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -5,6 +5,7 @@ export const initGA = () => {
     console.log(env.VITE_PRODUCTION);
     console.log(env.VITE_GA_ID);
 
+
     if (env.VITE_PRODUCTION && env.VITE_GA_ID) {
         ReactGA.initialize(env.VITE_GA_ID);
     }
