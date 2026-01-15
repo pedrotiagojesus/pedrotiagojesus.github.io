e235e7d5d0127cab133de5f731a1154a77a17778
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2026-01-15 23:40:30 +0000

    Add Google Analytics integration and tracking events

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -2,10 +2,6 @@ import { env } from "@config/env";
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-    console.log(env.VITE_PRODUCTION);
-    console.log(env.VITE_GA_ID);
-
-
     if (env.VITE_PRODUCTION && env.VITE_GA_ID) {
         ReactGA.initialize(env.VITE_GA_ID);
     }
