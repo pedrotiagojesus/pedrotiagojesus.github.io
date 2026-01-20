f9fc3fc1aa3984ea2429cde40ff4776bab855790
pedrotiagojesus <pedrotiagojesus1995@gmail.com>
2026-01-20 22:42:49 +0000

    API update

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,5 +1,5 @@
 import { env } from "@config/env";
 
 export const getProjectImage = (image: string) => {
-    return env.VITE_ENDPOINT_PUBLIC.concat("uploads/projects/", image)
+    return env.VITE_ENDPOINT_PUBLIC.concat("projects/", image)
 };
